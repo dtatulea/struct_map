4debae6068a7bb2f306360c3c2d763a8209675cd
Dragos Tatulea <dragos.tatulea@gmail.com>
2011-10-04 09:30:33 +0300

    test: iterate through structs and print them
    Signed-off-by: Dragos Tatulea <dragos.tatulea@gmail.com>

--- struct_map.c
+++ struct_map.c
@@ -26,13 +26,24 @@
     [] walk structure by structure in depth, mark input structures
     [] keep structure as tree (not rbtree)
     [] generate .dot file
+
+  Q's:
+    [] how to iterate through structs
+    [] how to go deep in structs
+    [] how to hash a struct name, or hash by smth else?
 */
 
 #include <argp.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <stdio.h>
 #include <dwarf.h>
+
 #include "list.h"
+#include "dwarves.h"
+#include "dutil.h"
 
 /* Name and version of program.  */
 ARGP_PROGRAM_VERSION_HOOK_DEF = dwarves_print_version;
@@ -40,6 +51,8 @@ ARGP_PROGRAM_VERSION_HOOK_DEF = dwarves_print_version;
 #define MAX_FILENAME_LEN 255
 char output_file[MAX_FILENAME_LEN] = "struct_map.dot";
 
+static struct conf_load conf_load;
+#if 0
 /** smap hash **/
 struct smap_struct_hash_node {
 	struct hlist_head hash_node;
@@ -73,10 +86,34 @@ static int smap__hash_add_node(char *struct_name)
 }
 
 /** smap hash **/
+#endif
 
-static int smap__parse()
+void print_variable(struct cu *cu, struct variable *var)
 {
+	struct ip_tag *itag = &var->ip;
+	struct tag *tag  = &itag->tag;
+
+	tag__fprintf(tag, cu, NULL, stdout);
+	putchar('\n');
+	printf("var::external:%d, decl:%d || ", var->external, var->declaration);
+	printf("ip_tag:: addr:%p || ", itag->addr);
+	printf("tag:: type:0x%x, tag:%d, visited:%d, top_level:%d, recursivity_level:%d\n",
+		tag->type, tag->tag, tag->visited, tag->top_level, tag->recursivity_level);
 }
+
+static int cu_struct_iterator(struct cu *cu, void *cookie __unused)
+{
+	struct tag *pos;
+	uint32_t id;
+
+	cu__for_each_type(cu, id, pos) {
+		struct variable *var = tag__variable(pos);
+		print_variable(cu, var);
+	}
+
+	return 0;
+}
+
 
 static const struct argp_option smap__options[] = {
 	{
@@ -89,7 +126,7 @@ static const struct argp_option smap__options[] = {
 	{
 		.key  = 'o',
 		.name = "output_name",
-		.arg  = "OUTPUT_FILE"
+		.arg  = "OUTPUT_FILE",
 		.doc  = "set name of output file",
 	},
 	{
@@ -109,7 +146,7 @@ static smap__parse_structs(char *argp)
 		}
 
 		argp++;
-	} while (argp)
+	} while (argp);
 }
 
 static error_t smap__options_parser(int key, char *arg,
@@ -117,7 +154,7 @@ static error_t smap__options_parser(int key, char *arg,
 {
 	switch (key) {
 	case 's':
-		assert(1);
+		assert(0);
 		break;
 	case 'o':
 		assert(strlen(arg) < MAX_FILENAME_LEN);
@@ -139,7 +176,36 @@ static struct argp smap__argp = {
 
 int main(int argc, char *argv[])
 {
+	int err, remaining, rc = EXIT_FAILURE;
 
-	assert(struct_list_parsed);
+	if (argp_parse(&smap__argp, argc, argv, 0, &remaining, NULL) ||
+	    remaining == argc) {
+                argp_help(&smap__argp, stderr, ARGP_HELP_SEE, argv[0]);
+                goto out;
+	}
+
+	if (dwarves__init(0)) {
+		fputs("smap: insufficient memory\n", stderr);
+		goto out;
+	}
+
+	struct cus *cus = cus__new();
+	if (cus == NULL) {
+		fputs("smap: insufficient memory\n", stderr);
+		goto out_dwarves_exit;
+	}
+
+	printf("remaining:%d\n", remaining);
+	err = cus__load_files(cus, &conf_load, argv + remaining);
+	if (err != 0)
+		goto out_cus_delete;
+
+	cus__for_each_cu(cus, cu_struct_iterator, NULL, NULL);
+
+out_cus_delete:
+	cus__delete(cus);
+out_dwarves_exit:
+	dwarves__exit();
+out:
 	return 0;
 }
